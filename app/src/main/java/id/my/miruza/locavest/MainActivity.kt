0a3790115761a2fe0929253fde49ba1c4d653385
mirza <miruza.dev@gmail.com>
2024-04-14 19:10:26 +0700

    feat: View bind main layout

--- app/src/main/java/id/my/miruza/locavest/MainActivity.kt
+++ app/src/main/java/id/my/miruza/locavest/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import id.my.miruza.locavest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
